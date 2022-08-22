f6874c67d6270737e13ee16cfc6aca4c74667073
zhaochengxian <zcxzmzy@163.com>
2022-08-22 15:27:56 +0800

    feat: 引入redux

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from './reducers'
+
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const middlewares = [thunkMiddleware]
+
+const enhancer = composeEnhancers(
+    applyMiddleware(...middlewares),
+
+)
+
+export default function configStore() {
+    const store = createStore(rootReducer, enhancer)
+    return store
+}
---
+++ src/store/reducers/index.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+
+// import loading from './loading'
+
+export default combineReducers({
+
+})
