6005999c8f1b49cb0191b66628309492ec5d7d2b
zhaochengxian <zcxzmzy@163.com>
2022-08-24 17:18:40 +0800

    feat: 配置不同的环境开关，支持连接后端本地

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,38 @@
-import { defineConfig } from "vite";
+import * as path from "path";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
 
 export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
     return defineConfig({
-        plugins: [react()],
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src')
-            }
+        plugins: [
+            react(),
+        ],
+        server: {
+            host: "0.0.0.0",
+            // port: 4000,
+            open: true,
+            strictPort: false,
+            proxy: {
+                "/api": {
+                    target: env?.VITE_BASE_API, //动态获取不同环境的api,并兼容直接连接后台本地
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ""), // 路径重写
+                },
+            },
         },
-        define: {
-            "process.env.NODE_ENV": `"${mode}"`,
-        }
-    })
+
+        resolve: {
+            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svg'],
+            alias: {
+                '@': path.resolve(__dirname, '/src'),
+                '@c': path.resolve(__dirname, '/src/components'),
+                '@v': path.resolve(__dirname, '/src/views'),
+                '@assets': path.resolve(__dirname, '/src/assets'),
+                'venn.js': 'venn.js/build/venn.js',
+            },
+        },
+    });
+
 }
+
