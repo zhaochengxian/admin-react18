36acf1992edb1865a3b9d1ef4d11597b75a7a88e
zhaochengxian <zcxzmzy@163.com>
2022-08-25 14:28:19 +0800

    feat: 引入eslint管理代码质量

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"],
+        "indent": [2, 4],// 锁进
+        "init-declarations": 0,//声明时必须赋初值
+        "default-case": 2,//switch语句最后必须有default
+        "comma-dangle": [2, "never"],//对象字面量项尾不能有逗号
+        "no-use-before-define": 2,//未定义前不能使用
+        "no-useless-call": 2,//禁止不必要的call和apply
+        "no-unreachable": 2,//不能有无法执行的代码
+        "no-unused-expressions": 2,//禁止无用的表达式
+        "no-shadow": 2,//外部作用域中的变量不能与它所包含的作用域中的变量或参数同名
+        "no-redeclare": 2,//禁止重复声明变量
+        "no-fallthrough": 1,//禁止switch穿透
+        "no-const-assign": 2,//禁止修改const声明的变量
+
+    },
+    "ignorePatterns": [
+        'node_modules',
+        'build',
+        'dist',
+        'tests'
+    ],
+}
