708f4291290377d4aa99c3b8fb040def9b378937
zhaochengxian <zcxzmzy@163.com>
2022-08-26 10:52:20 +0800

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { useRoutes } from "react-router-dom";
-import routes from "./router";
+import React from 'react'
+import { useRoutes } from 'react-router-dom'
+import routes from './router'
 
-import "./App.css";
-
+import './global.scss'
+//
 function App() {
-  let element = useRoutes(routes);
-  return <div className="App">{element}</div>;
+    const element = useRoutes(routes)
+
+    return <div className="App">{element}</div>
 }
 
-export default App;
+export default App
